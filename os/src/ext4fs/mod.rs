c768e8e215072193afde9edaa5eb5376ac944f77
idream <3556495919@qq.com>
2026-01-28 17:10:02 +0800

    1

---
+++ os/src/ext4fs/mod.rs
@@ -0,0 +1,14 @@
+mod bitmap;
+mod block_cache;
+mod block_dev;
+//mod efs;
+mod layout;
+//mod vfs;
+pub mod superblock;
+pub const BLOCK_SZ: usize = 512;
+//use bitmap::Bitmap;
+use block_cache::{get_block_cache};
+pub use block_dev::BlockDevice;
+//pub use efs::EasyFileSystem;
+//use layout::*;
+//pub use vfs::Inode;
