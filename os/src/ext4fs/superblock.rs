7921e5aaef322f023d4a52ddd77bc27480e133ed
idream <3556495919@qq.com>
2026-01-29 10:33:51 +0800

    改写了驱动，此时驱动可以根据传入的缓冲区大小自动读取从指定block_id开始的数据块，缓冲区除以512并向下取整

--- os/src/ext4fs/superblock.rs
+++ os/src/ext4fs/superblock.rs
@@ -16,8 +16,11 @@ pub struct Ext4SuperBlock {
 
 impl Ext4SuperBlock {
     pub fn new(ext4_superblock_disk: Ext4SuperBlockDisk) -> Self {
-        if ext4_superblock_disk.s_magic != EXT4_MAGIC as u16 {
-            panic!("Not a valid ext4 superblock: magic = ");
+        let magic = ext4_superblock_disk.s_magic;   //防止未对齐引用导致的编译问题
+        println!("EXT4 Magic from disk: 0x{:X}", magic);
+        
+        if magic != EXT4_MAGIC as u16 {
+            panic!("Not a valid ext4 superblock: magic = 0x{:X}", magic);
         }
         Self {
             total_blocks: ext4_superblock_disk.s_blocks_count_lo,
