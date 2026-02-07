92944b40cb18170143dd3dbd1b18b5ac696149cc
fnnx1 <FMxi666@outlook.com>
2026-02-08 00:15:42 +0800

    完善la架构的trap实现

--- os/src/arch/la/config.rs
+++ os/src/arch/la/config.rs
@@ -11,7 +11,7 @@ pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 /// kernel heap size
 pub const KERNEL_HEAP_SIZE: usize = 0x200_0000;
 
-/// page size : 4KB
+/// page size : 4KB 
 pub const PAGE_SIZE: usize = 0x1000;
 /// page size bits: 12
 pub const PAGE_SIZE_BITS: usize = 0xc;
