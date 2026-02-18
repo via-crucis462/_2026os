701aa7e5ca79be5c096dfba1125fc443255b9e87
fnnx1 <fmxi666@outlook.com>
2026-02-18 18:42:18 +0800

    为pci和vitio_block的转换实现接口

--- os/src/arch/la/drivers/block/mod.rs
+++ os/src/arch/la/drivers/block/mod.rs
@@ -2,22 +2,28 @@
 
 mod virtio_blk;
 
-pub use virtio_blk::VirtIOBlock;
+pub use virtio_blk::*;
+
 
 use crate::{ext4fs::BlockDevice, sync::UPSafeCell};
 use lazy_static::*;
+#[allow(unused)]
+use crate::arch::drivers::pci;
 
 type BlockDeviceImpl = virtio_blk::VirtIOBlock;
 
 lazy_static! {
     /// The global block device driver instance: BLOCK_DEVICE with BlockDevice trait
-    pub static ref BLOCK_DEVICE: UPSafeCell<BlockDeviceImpl> = unsafe { UPSafeCell::new(BlockDeviceImpl::new()) };
+    pub static ref BLOCK_DEVICE: UPSafeCell<BlockDeviceImpl>
+        = unsafe { UPSafeCell::new(BlockDeviceImpl::new(
+            pci::scan_and_init_pci_device().expect("Failed to initialize PCI device")
+        )) };
 }
 
 #[allow(unused)]
 /// Test the block device
-pub fn block_device_test() {
-    let block_device = BLOCK_DEVICE.clone();
+pub unsafe fn block_device_test() {
+    let mut block_device = BLOCK_DEVICE.exclusive_access();
     let mut write_buffer = [0u8; 512];
     let mut read_buffer = [0u8; 512];
     for i in 0..512 {
