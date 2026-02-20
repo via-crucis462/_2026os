0420b4a9a8ffe88c2d89b749ed12dcc78cb9227c
fnnx1 <fmxi666@outlook.com>
2026-02-20 23:24:08 +0800

    修复了上一次修改导致的rv64版本编译错误

--- os/src/arch/riscv/config.rs
+++ os/src/arch/riscv/config.rs
@@ -21,5 +21,7 @@ pub const TRAP_CONTEXT_BASE: usize = TRAMPOLINE - PAGE_SIZE;
 pub const CLOCK_FREQ: usize = 12500000;
 /// the physical memory end
 pub const MEMORY_END: usize = 0x88000000;
+/// 这里也定义一个
+pub const DMA_SIZE: usize = 0;
 /// The base address of control registers in Virtio_Block device
 pub const MMIO: &[(usize, usize)] = &[(0x10001000, 0x1000)];
