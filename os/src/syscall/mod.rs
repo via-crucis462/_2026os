26de7294489079733ca62e01ab1cc44a1581cde2
fnnx1 <fmxi666@outlook.com>
2026-03-02 21:17:37 +0800

    调整初始化程序为shell，修改部分调试信息

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -89,7 +89,7 @@ use crate::{fs::Stat, task::SignalAction};
 /// handle syscall exception with `syscall_id` and other arguments
 
 pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
-    info!("[kernel] syscall: id={}, args={:?}", syscall_id, args);
+    //info!("[kernel] syscall: id={}, args={:?}", syscall_id, args);
     match syscall_id {
         SYSCALL_DUP => sys_dup(args[0]),
         SYSCALL_DUP2 => sys_dup2(args[0], args[1]),
