5209e14cde0af2777e44025c0ce6804401c0964d
idream <3556495919@qq.com>
2026-03-11 08:28:02 +0800

    修改brk的地址转换为isize

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -122,9 +122,9 @@ use crate::{fs::Stat, task::SignalAction};
 /// handle syscall exception with `syscall_id` and other arguments
 
 pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
-    /*if syscall_id != 64 && syscall_id != 63 {
+    if syscall_id != 64 && syscall_id != 63 {
         println!("[Syscall Trace] ID: {}, args: {:#x?}", syscall_id, args);
-    }*/
+    }
     //info!("[kernel] syscall: id={}, args={:?}", syscall_id, args);
     match syscall_id {
         SYSCALL_DUP => sys_dup(args[0]),
