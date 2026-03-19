44c4990d47b26b77744f47049f302770d51ef00f
fnnx1 <fmxi666@outlook.com>
2026-03-19 16:31:41 +0800

    修改qemu运行时的sbi

--- os/src/arch/riscv/sbi.rs
+++ os/src/arch/riscv/sbi.rs
@@ -5,12 +5,12 @@
 use core::arch::asm;
 
 // 如果用qemu8，下列需要修改
-const SBI_SET_TIMER: usize = 0;//qemu7
-// const SBI_SET_TIMER: usize = 0x54494D45;//qemu8
+// const SBI_SET_TIMER: usize = 0;//qemu7
+const SBI_SET_TIMER: usize = 0x54494D45;//qemu8
 const SBI_CONSOLE_PUTCHAR: usize = 1;
 const SBI_CONSOLE_GETCHAR: usize = 2;
-const SBI_SHUTDOWN: usize = 8;//qemu7
-// const SBI_SHUTDOWN: usize = 0x53525354;//qemu8
+// const SBI_SHUTDOWN: usize = 8;//qemu7
+const SBI_SHUTDOWN: usize = 0x53525354;//qemu8
 
 /// general sbi call
 #[inline(always)]
