8faf8c55344e6fae489ec567e8711e6a4eb129b3
idream <3556495919@qq.com>
2026-04-14 20:52:51 +0800

    完成了stderr的补充

--- os/src/fs/stdio.rs
+++ os/src/fs/stdio.rs
@@ -24,7 +24,8 @@ pub struct Stdin;
 
 /// stdout file for putting chars to console
 pub struct Stdout;
-
+/// stderr file for putting chars to console
+pub struct Stderr;
 impl File for Stdin {
     fn readable(&self) -> bool {
         true
@@ -138,6 +139,41 @@ impl File for Stdout {
     }
     fn getdents(&self, _buf: &mut [u8]) -> isize {
         trace!("Stdout: getdents called on stdout, returning -1");
+        -1
+    }
+}
+
+impl File for Stderr {
+    fn readable(&self) -> bool {
+        false
+    }
+    fn writable(&self) -> bool {
+        true
+    }
+    fn read(&self, _user_buf: UserBuffer) -> usize {
+        panic!("Cannot read from stderr!");
+    }
+    fn write(&self, user_buf: UserBuffer) -> usize {
+        for buffer in user_buf.buffers.iter() {
+            println!("{}", core::str::from_utf8(*buffer).unwrap());
+        }
+        user_buf.len()
+    }
+    fn read_at(&self, _offset: usize, buf: UserBuffer) -> usize {
+        self.read(buf)
+    }
+    fn write_at(&self, _offset: usize, buf: UserBuffer) -> usize {
+        self.write(buf)
+    }
+    fn get_stat(&self) -> super::Stat {
+        super::Stat {
+            mode: 0o020000,
+            blksize: 4096,
+            ..Default::default()
+        }
+    }
+    fn getdents(&self, _buf: &mut [u8]) -> isize {
+        trace!("Stderr: getdents called on stderr, returning -1");
         -1
     }
 }
--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -308,7 +308,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             Errno::ENOSYS.as_isize()}
     };
     if syscall_id != SYSCALL_WRITE && syscall_id != SYSCALL_READ && syscall_id != SYSCALL_WRITEV && syscall_id != SYSCALL_READV {
-        println!(
+        debug!(
             "[Syscall Trace] ID: {:3} | Args: [{:#x}, {:#x}, {:#x}] | Ret: {}", 
             syscall_id, args[0], args[1], args[2], ret
         );
