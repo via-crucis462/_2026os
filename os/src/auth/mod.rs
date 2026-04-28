8e3deb3ae1874abfa4376608bce190d745e5dde8
fnnx1 <fmxi666@outlook.com>
2026-04-28 21:12:24 +0800

    继续修改perm相关

--- os/src/auth/mod.rs
+++ os/src/auth/mod.rs
@@ -1,5 +1,10 @@
 //! 权限管理相关
+use alloc::task;
 
+use crate::process::current_task;
+
+
+/// 文件权限信息，用于文件长期记录
 pub struct PermStat {
     pub mode: u32,     // 权限 + 文件类型
     pub uid: u32,      // 所有者
@@ -7,6 +12,7 @@ pub struct PermStat {
 }
 
 impl PermStat {
+    /// 初始化默认权限，默认全权限，root 用户和 root 组
     pub fn init_all_perm() -> Self {
         Self {
             mode: 0o777, // 默认全权限
@@ -14,6 +20,7 @@ impl PermStat {
             gid: 0,      // root 组
         }
     }
+    /// 指定权限、用户和组
     pub fn new(mode: u32, uid: u32, gid: u32) -> Self {
         Self { mode, uid, gid }
     }
@@ -25,6 +32,20 @@ impl PermStat {
     }
     pub fn set_mode(&mut self, mode: u32) {
         self.mode = mode;
+    }
+    /// 获取当前用户对目标文件的权限集合
+    pub fn current_perm_set(&self) -> PermSet {
+        let proc = current_task().unwrap().process.upgrade().unwrap();
+        let inner = proc.inner_exclusive_access();
+        let uid = inner.uid;
+        let gid = inner.gid;
+        drop(inner);
+        drop(proc);
+        PermSet {
+            writeable: self.can_write(uid, gid),
+            readable: self.can_read(uid, gid),
+            executable: self.can_execute(uid, gid),
+        }
     }
     pub fn can_read(&self, uid: u32, gid: u32) -> bool {
         if uid == 0 || self.uid == uid {
@@ -53,4 +74,11 @@ impl PermStat {
             self.mode & 0o001 != 0
         }
     }
+}
+
+/// 当前用户对某个文件的权限，仅用于临时存储
+pub struct PermSet{
+    pub writeable: bool,
+    pub readable: bool,
+    pub executable: bool,
 }
